63fe9114cd4799a8d8019e97cb8c18192b16ace0
anwholesquare <anwholesquare@gmail.com>
2023-09-01 03:39:17 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutterchat
+Test Chat UI based on flutter_chat_ui: ^1.6.9
---
+++ android/app/src/main/kotlin/com/khandokeranan/flutterchat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.khandokeranan.flutterchat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
